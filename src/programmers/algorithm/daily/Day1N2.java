a9e915580744142761a1e43242b91a6eab8b6a66
seoyun <may-primavera@naver.com>
2022-10-23 23:58:23 +0900

    feat: 숫자 비교하기

---
+++ src/programmers/algorithm/daily/Day1N2.java
@@ -0,0 +1,13 @@
+package programmers.algorithm.daily;
+
+public class Day1N2 {
+    int solution(int num1, int num2) {
+        int answer = -1;
+
+        if (num1 == num2) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
