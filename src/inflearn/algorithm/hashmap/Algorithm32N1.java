2d3f0c433a5720641820496feb0baf5ec8f442e8
seoyunjang <may-primavera@naver.com>
2023-03-31 23:56:59 +0900

    feat: hashmap
    - 학생 회장

---
+++ src/inflearn/algorithm/hashmap/Algorithm32N1.java
@@ -0,0 +1,14 @@
+package inflearn.algorithm.hashmap;
+
+import java.util.Scanner;
+
+public class Algorithm32N1 {
+    public int solution(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Algorithm32N1 T = new Algorithm32N1();
+        Scanner sc = new Scanner(System.in);
+    }
+}
