1a3ce8af265ee87b5cd4ba78494997c58f76209e
seoyunjang <may-primavera@naver.com>
2023-04-03 00:39:35 +0900

    feat: 학급 회장(해쉬)

--- src/inflearn/algorithm/hashmap/Algorithm32N1.java
+++ src/inflearn/algorithm/hashmap/Algorithm32N1.java
@@ -1,14 +1,33 @@
 package inflearn.algorithm.hashmap;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Algorithm32N1 {
-    public int solution(int n) {
-        return 0;
+    public char solution(int n, char[] arr) {
+        int cnt = Integer.MIN_VALUE;
+        char answer = ' ';
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : arr) {//count 할 때 getOrDefault 사용한다.
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for(char key : map.keySet()){
+            if (map.get(key) > cnt) {
+                cnt = map.get(key);
+                answer = key;
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Algorithm32N1 T = new Algorithm32N1();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] arr = sc.next().toCharArray();
+        System.out.println(T.solution(n, arr));
     }
 }
