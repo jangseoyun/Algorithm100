e5ca41f8af9216a1e8f66f0b85a4acfea113261c
seoyunjang <may-primavera@naver.com>
2023-04-28 00:08:47 +0900

    feat: 버블정렬

---
+++ src/inflearn/algorithm/sorting/Algorithm46N2.java
@@ -0,0 +1,33 @@
+package inflearn.algorithm.sorting;
+
+import java.util.Scanner;
+
+public class Algorithm46N2 {
+    public int[] solution(int n, int[] arr){
+        int tmp = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+    public static void main(String[] args){
+        Algorithm46N2 T = new Algorithm46N2();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
